af3bef32531b4e3aabbe9f7703cc013d2f831e7d
pauldoazan <p.doazan@lehibou.com>
2025-08-20 23:27:48 +0200

    Add Tailwind CSS Vite plugin and Athelas font integration; update Home component layout and styles

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,39 @@
 <script setup lang="ts">
-
+import ArrowIn from './Icon/ArrowIn.vue'
 </script>
 
 <template>
-  <div class="w-screen h-screen bg-background">
-    <div class="grid grid-cols-3">
-      <div class="bg-creme">
-        <h1>Borne ressource</h1>
+  <div class="w-screen bg-background">
+    <div class="h-screen grid grid-cols-3">
+      <div class="bg-creme flex items-center justify-center">
+        <div class="h-84 w-100 flex flex-col justify-between transform -translate-y-12">
+          <div class="font-bold leading-20" style="font-size: 40px;">
+            Borne ressource
+          </div>
+          <div class="font-soleil leading-9.5 text-2xl" style="font-size: 28px;">
+            Accédez à une sélection de fiches explicatives et de vidéos pour découvrir le patrimoine, les filières,
+            les
+            démarches collectives et les productions agricoles du territoire.
+          </div>
+        </div>
       </div>
-      <div class="bg-kaki">
-        <h1>Fiches explicatives</h1>
+      <div class="bg-kaki flex items-center justify-center">
+        <div class="h-84 flex flex-col items-center justify-between transform -translate-y-14">
+          <div class="text-2xl font-athelas text-center leading-20" style="font-size: 70px;">
+            Fiches<br>explicatives
+          </div>
+          <div>
+            <ArrowIn />
+          </div>
+        </div>
       </div>
-      <div class="bg-lightkaki">
-        <h1>Vidéothèque</h1>
+      <div class="bg-lightkaki flex items-center justify-center">
+        <div class="h-84 flex flex-col items-center justify-between transform -translate-y-14">
+          <div class="text-2xl font-athelas text-center leading-20" style="font-size: 70px;">
+            Vidéothèque
+          </div>
+          <ArrowIn />
+        </div>
       </div>
     </div>
   </div>
---
+++ src/components/Icon/ArrowIn.vue
@@ -0,0 +1,26 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="103.013" height="127.955" viewBox="0 0 103.013 127.955">
+    <g id="Group_51" data-name="Group 51" transform="translate(0 0)">
+      <path id="Path_138" data-name="Path 138"
+        d="M60.829,0V0H38.667v8.43H60.829V119.525H38.667v8.43H69.261v-8.43h0V8.432h0V0h0V0Z"
+        transform="translate(33.752 0)" fill="#040405" />
+      <path id="Path_139" data-name="Path 139"
+        d="M40.414,22.117,64.035,45.74H0v8.428H64L40.412,77.753l5.961,5.963,33.78-33.78v0L46.374,16.157Z"
+        transform="translate(0 14.104)" fill="#040405" />
+    </g>
+  </svg>
+</template>
+
+<script setup lang="ts">
+// Props for customization
+interface Props {
+  width?: string | number
+  height?: string | number
+  class?: string
+}
+
+withDefaults(defineProps<Props>(), {
+  width: '103.013',
+  height: '127.955'
+})
+</script>
