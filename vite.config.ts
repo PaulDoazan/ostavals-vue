7eb41cf921a57738b926758ec802c56d02370ede
pauldoazan <p.doazan@lehibou.com>
2025-08-25 00:02:35 +0200

    Update configuration and HTML for GitHub Pages deployment
    - Set the base path in vite.config.ts to use relative paths for GitHub Pages.
    - Updated index.html to reference assets with relative paths for proper loading.
    - Added new CSS file for styling enhancements.
    - Introduced new JavaScript file for improved functionality.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // Use relative paths for GitHub Pages
   plugins: [
     vue(),
   ],
